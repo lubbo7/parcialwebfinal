8c258ab553ada73206015fda62aad7db7114150f
lubbo7 <andresgarciaerira@gmail.com>
2018-05-25 11:19:40 -0500

    links working

--- index.js
+++ index.js
@@ -12,6 +12,18 @@ app.get('/', (req, res) => {
     
     res.render('home');
     });
+    
+    app.get('/home',(req,res)=>{
+        res.render('home');
+    });
+
+    app.get('/reachus',(req,res)=>{
+        res.render('reachus');
+    });
+
+    app.get('/help',(req,res)=>{
+        res.render('help');
+    });
 
 app.listen(3000, () => {
     console.log("Puerto working duh");
